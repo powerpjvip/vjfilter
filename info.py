db433a3254c6dacec33276988bb62c6ef14f1de8
powerpjvip <154010091+powerpjvip@users.noreply.github.com>
2024-05-25 16:35:48 +0530

    Update info.py

--- info.py
+++ info.py
@@ -137,11 +137,11 @@ RENAME_MODE = bool(environ.get('RENAME_MODE', True)) # Set True or Flase
 AUTO_APPROVE_MODE = bool(environ.get('AUTO_APPROVE_MODE', True)) # Set True or Flase
 
 # Save Restricted Info : If True Then Bot Save Content From Restricted Channel Else Not
-SAVE_RESTRICTED_MODE = bool(environ.get('SAVE_RESTRICTED_MODE', False)) # Set True or Flase
+SAVE_RESTRICTED_MODE = bool(environ.get('SAVE_RESTRICTED_MODE', True)) # Set True or Flase
 
 # if SAVE_RESTRICTED_MODE is True Then Fill String Session Variable In Your Server Environment Variable, If Flase Then No Need To Fill.
 # Warning: Never Fill String Session Variable In Your Repo, If You Fill Then Your Account Can Be Access By Anyone.
-SESSION_STRING = environ.get('SESSION_STRING', '') # PYROGRAM V2 Session 
+SESSION_STRING = environ.get('SESSION_STRING', 'AQHD6cYAiVbEHw_nHhCkwUDAo50faE7lC9Rb2Ygw-l-epxYoSFqYSNlMHBIBY-CzmPTsVM5WiyMOYjOAQGBARgHwYdB5bmIZgVxi3P3jyhrkdiF-MZbwJn_9G-qHfvpLwJ_ABME6NSolBT1lEJWoS1h3jLYroiUh4JavOq06Om0nWoocFS3mMlMRY_mAGg41HlxmIdi9cTo-62iSB76G6rjtGyzOTlLcn_esmEorSvP6JVPvaj6Dnod6OBDkPTbt9YHozih0VStFZMGkZ4RP9JFb85IuTMr_769NWkaOVJTSgYm-UK308VrMZNfIrySPtk1laTlpAk5cf2vKmDJSKilrERuDRwAAAAFUSGbmAA') # PYROGRAM V2 Session 
 
 LOG_STR = "Current Cusomized Configurations are:-\n"
 LOG_STR += ("IMDB Results are enabled, Bot will be showing imdb details for you queries.\n" if IMDB else "IMBD Results are disabled.\n")
